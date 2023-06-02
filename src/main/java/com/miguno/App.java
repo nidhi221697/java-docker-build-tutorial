bd8b98c148b3dd8dde207748400156f6387e2906
Nidhi <113484706+nidhi221697@users.noreply.github.com>
2023-06-02 16:37:37 +0530

    Update App.java

--- src/main/java/com/miguno/App.java
+++ src/main/java/com/miguno/App.java
@@ -1,8 +1,8 @@
 package com.miguno;
 
-import org.glassfish.grizzly.http.server.HttpServer;
+#import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
+#import org.glassfish.jersey.server.ResourceConfig;
 
 import java.net.URI;
 
